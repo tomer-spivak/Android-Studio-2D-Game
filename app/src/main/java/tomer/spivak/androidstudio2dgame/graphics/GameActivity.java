f24ccddbec6ab4966977174fdb65855f8a37c3b4
tomer-spivak <spivak.toti@gmail.com>
2025-05-27 21:42:35 +0300

    bagrut

--- app/src/main/java/tomer/spivak/androidstudio2dgame/graphics/GameActivity.java
+++ app/src/main/java/tomer/spivak/androidstudio2dgame/graphics/GameActivity.java
@@ -60,7 +60,6 @@ public class GameActivity extends AppCompatActivity implements GameEventListener
 
     private GameView gameView;
 
-
     private GameViewModel viewModel;
 
     private SoundEffectManager soundEffectsManager;
